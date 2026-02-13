43d39e04aa0825052a5a2d67be6bbbe03c230d6f
Supachok Julaupay <supachok.jul@ku.th>
2026-02-13 14:44:02 +0700

    Test React component: TodoItem

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,                          // ทำให้เรียกฟังก์ชันเกี่ยวกับการเทสได้โดยไม่ต้องประกาศ
+    environment: 'jsdom',                   // รันเทสแบบไม่มี browser
+    setupFiles: './src/setupTests.js',      // ระบุโค้ดสำหรับเตรียมต่าง ๆ
+  },
   server: {
     proxy: {
       '/api': {
