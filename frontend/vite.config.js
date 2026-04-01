d01d3ae64c31b72deeb00d3ac1efb1098eeaf4eb
Supachok Julaupay <supachok.jul@ku.th>
2026-04-01 10:14:13 +0700

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,9 +11,13 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:5000',
-        changeOrigin: true
-      }
-    }
-  }
+        target: 'http://localhost:8080/',
+        changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    outDir: '../backend/frontend-static',
+    emptyOutDir: true,
+  },
 })
